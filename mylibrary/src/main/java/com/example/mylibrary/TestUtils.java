03a5233f659b34e77e4f299394173272fe8eede4
Carlos <mrcarlosyang@gmail.com>
2020-11-17 16:41:15 -0500

    test

---
+++ mylibrary/src/main/java/com/example/mylibrary/TestUtils.java
@@ -0,0 +1,12 @@
+package com.example.mylibrary;
+
+/**
+ * Created on 11/17/2020 Tue
+ *
+ * @author Chuliang
+ */
+class TestUtils {
+    public static String output(){
+        return "this is a test";
+    }
+}
