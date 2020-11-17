955d671dfd9a9aa2905f69c5c50d8023edcae26d
Carlos <mrcarlosyang@gmail.com>
2020-11-16 20:51:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/TestUtils.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+/**
+ * Created on 11/16/2020 Mon
+ *
+ * @author Chuliang
+ */
+class TestUtils {
+    public static String  test(){
+        return "test";
+    }
+}
